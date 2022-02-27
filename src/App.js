1bd9c4523ada35798a2105a1e48df4d66d9050bc
Manish Jha <manishjhazyx@gmail.com>
2022-02-27 14:35:52 +0530

    feat: infinite scroll, top loading bar

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
   pageSize = 15;
+  state = {
+    progress: 10,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <BrowserRouter>
         <div>
           <Navbar />
-
+          <LoadingBar
+            color="#f11946"
+            progress={this.state.progress}
+            // onLoaderFinished={() => setProgress(0)}
+          />
           <Routes>
             <Route
               index
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="default"
                   pageSize={this.pageSize}
                   country="in"
@@ -28,6 +40,7 @@ export default class App extends Component {
               path="/Business"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="business"
                   pageSize={this.pageSize}
                   country="in"
@@ -39,6 +52,7 @@ export default class App extends Component {
               path="/General"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="general"
                   pageSize={this.pageSize}
                   country="in"
@@ -50,6 +64,7 @@ export default class App extends Component {
               path="/Entertainment"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="entertainment"
                   pageSize={this.pageSize}
                   country="in"
@@ -61,6 +76,7 @@ export default class App extends Component {
               path="/Science"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="science"
                   pageSize={this.pageSize}
                   country="in"
@@ -72,6 +88,7 @@ export default class App extends Component {
               path="/Sports"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="sports"
                   pageSize={this.pageSize}
                   country="in"
@@ -83,6 +100,7 @@ export default class App extends Component {
               path="/Health"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="health"
                   pageSize={this.pageSize}
                   country="in"
@@ -94,6 +112,7 @@ export default class App extends Component {
               path="/Technology"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="technology"
                   pageSize={this.pageSize}
                   country="in"
--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export class News extends Component {
   static defaultProps = {
     country: "in",
@@ -14,79 +16,118 @@ export class News extends Component {
     category: PropTypes.string,
   };
 
-  constructor() {
-    super();
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
+      totalResults: 0,
     };
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )} - NewsApp `;
   }
 
   async updateNews(pageNumber) {
+    this.props.setProgress(0);
     const url = `https://newsapi.org/v2/top-headlines?&country=${this.props.country}&category=${this.props.category}&apiKey=18753f77ed8d4a6688803545cfb985a8&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     console.log(`total articles:${parsedData.totalResults}`);
     await this.setState({
       articles: parsedData.articles,
       totalArticles: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
     await this.updateNews();
   }
 
-  handlePrevClick = async () => {
-    await this.setState({ page: this.state.page - 1 });
-    await this.updateNews();
-  };
+  //   handlePrevClick = async () => {
+  //     await this.setState({ page: this.state.page - 1 });
+  //     await this.updateNews();
+  //   };
 
-  handleNextClick = async () => {
-    await this.setState({ page: this.state.page + 1 });
-    await this.updateNews();
+  //   handleNextClick = async () => {
+  //     await this.setState({ page: this.state.page + 1 });
+  //     await this.updateNews();
+  //   };
+
+  fetchMoreData = async () => {
+    this.setState({
+      page: this.state.page + 1,
+    });
+    const url = `https://newsapi.org/v2/top-headlines?&country=${this.props.country}&category=${this.props.category}&apiKey=18753f77ed8d4a6688803545cfb985a8&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    // this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(`total articles:${parsedData.totalResults}`);
+    await this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalArticles: parsedData.totalResults,
+      loading: false,
+    });
   };
 
   render() {
     return (
-      <div className="container my-3">
+      <>
+        {/* <div className="container my-3"> */}
         <h1 className="text-center" style={{ margin: "40px 0px" }}>
-          NewsApp - Top Headlines
+          NewsApp - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
+          Headlines
         </h1>
-        {this.state.loading && <Spinner />}
-
-        <div className=" row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className=" d-flex justify-content-center col-md-4">
-                  <NewsItem
+        {/* {this.state.loading && <Spinner />} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalArticles}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className=" row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div
+                    className=" d-flex justify-content-center col-md-4"
                     key={element.url}
-                    title={element.title ? element.title : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : ""
-                    }
-                    imageUrl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/amp_blog_image_large/public/blog/csm-blog/breaking-news-blog-1138x658-1.jpg?itok=yqaRxbyn"
-                    }
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
+                  >
+                    <NewsItem
+                      key={element.url}
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88)
+                          : ""
+                      }
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/amp_blog_image_large/public/blog/csm-blog/breaking-news-blog-1138x658-1.jpg?itok=yqaRxbyn"
+                      }
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
 
-        <div className="container d-flex justify-content-between">
+        {/* <div className="container d-flex justify-content-between">
           <button
             disabled={this.state.page <= 1}
             onClick={this.handlePrevClick}
@@ -106,8 +147,9 @@ export class News extends Component {
           >
             Next &rarr;
           </button>
-        </div>
-      </div>
+        </div> */}
+        {/* </div> */}
+      </>
     );
   }
 }
